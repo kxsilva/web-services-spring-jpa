b11963e3087ea78afe37711a7cc469aca8749651
Kaynã Xavier <kayna.xavier@ufpe.br>
2022-08-08 21:50:49 -0300

    Product

--- src/main/java/com/firstspring/course/entities/Category.java
+++ src/main/java/com/firstspring/course/entities/Category.java
@@ -1,13 +1,16 @@
 package com.firstspring.course.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 
 @Entity
@@ -20,7 +23,11 @@ public class Category implements Serializable{
 	private Long id;
 	private String name;
 	
-	public Category() {
+	@Transient
+	private Set<Product> products = new HashSet<>();
+	
+			
+			public Category() {
 		
 	}
 
@@ -62,7 +69,10 @@ public class Category implements Serializable{
 		Category other = (Category) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
+
+	public Set<Product> getProducts() {
+		return products;
+	}
+
 	
 }
---
+++ src/main/java/com/firstspring/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.firstspring.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.firstspring.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
