672144eef3dd31f8668ed37037c4f430bb397ab8
Kaynã Xavier <kayna.xavier@ufpe.br>
2022-08-15 21:58:39 -0300

    Payment, one-to-one association

--- src/main/java/com/firstspring/course/config/TestConfig.java
+++ src/main/java/com/firstspring/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.firstspring.course.entities.Category;
 import com.firstspring.course.entities.Order;
 import com.firstspring.course.entities.OrderItem;
+import com.firstspring.course.entities.Payment;
 import com.firstspring.course.entities.Product;
 import com.firstspring.course.entities.User;
 import com.firstspring.course.entities.enums.OrderStatus;
@@ -83,6 +84,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 
 	}
 	
