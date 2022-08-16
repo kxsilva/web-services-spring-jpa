e4cd3cadf0f69e7ec2c3e701f9d30b368ff4e4d4
Kaynã Xavier <kayna.xavier@ufpe.br>
2022-08-15 21:21:38 -0300

    OrderItem, many-to-many association with extra attributes PART 2

--- src/main/java/com/firstspring/course/config/TestConfig.java
+++ src/main/java/com/firstspring/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.firstspring.course.entities.Category;
 import com.firstspring.course.entities.Order;
+import com.firstspring.course.entities.OrderItem;
 import com.firstspring.course.entities.Product;
 import com.firstspring.course.entities.User;
 import com.firstspring.course.entities.enums.OrderStatus;
 import com.firstspring.course.repositories.CategoryRepository;
+import com.firstspring.course.repositories.OrderItemRepository;
 import com.firstspring.course.repositories.OrderRepository;
 import com.firstspring.course.repositories.ProductRepository;
 import com.firstspring.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -71,6 +76,14 @@ public class TestConfig implements CommandLineRunner{
 
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
 	}
 	
 	
---
+++ src/main/java/com/firstspring/course/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.firstspring.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.firstspring.course.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+
+}
